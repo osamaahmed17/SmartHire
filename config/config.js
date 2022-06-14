88590134105de65656f8d4c7b420f65041c8d0b2
osamaahmed17 <osamaahmedtahir17@gmail.com>
2022-06-15 00:02:54 +0500

    Mongodb connected

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports=
     {
-        "dbString":"mongodb://osamaahmed17:osamaahmed17@hackfest-shard-00-00.bhrym.mongodb.net:27017,hackfest-shard-00-01.bhrym.mongodb.net:27017,hackfest-shard-00-02.bhrym.mongodb.net:27017/?ssl=true&replicaSet=atlas-574kto-shard-0&authSource=admin&retryWrites=true&w=majority",
+        "dbConnection":"mongodb://osamaahmed17:osamaahmed17@hackfest-shard-00-00.bhrym.mongodb.net:27017,hackfest-shard-00-01.bhrym.mongodb.net:27017,hackfest-shard-00-02.bhrym.mongodb.net:27017/?ssl=true&replicaSet=atlas-574kto-shard-0&authSource=admin&retryWrites=true&w=majority",
+       
     
         
     }
--- config/db.js
+++ config/db.js
@@ -3,22 +3,19 @@ const config = require('../config/config.js')
 
 
 
-let connectionDb = config.dbString
-
-
 const connectDb = async () => {
-    try {
-        const conn = await mongoose.connect(connectionDb, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-        })
-        console.log("MongoDB connected")
-    } catch (err) {
-        console.log(`Error: ${err.message}`.red)
-        process.exit(1)
-    }
+  try {
+    const conn = await mongoose.connect(config.dbConnection, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    console.log("MongoDB connected")
+  } catch (err) {
+    console.log(`Error: ${err.message}`.red)
+    process.exit(1)
+  }
 }
 
-module.exports = connectDb;
+module.exports= connectDb;
 
