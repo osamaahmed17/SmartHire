cede7821ddf10a18005571c8a325b1423b7aa34c
osamaahmed17 <osamaahmedtahir17@gmail.com>
2022-06-19 09:33:12 +0500

    Updated Backend Route

--- frontend/src/Backend.js
+++ frontend/src/Backend.js
@@ -1,2 +1,2 @@
-//export const API = `https://smarthire.herokuapp.com/`;
-export const API = `http://localhost:5000/`;
+export const API = `https://smarthire.herokuapp.com/`;
+//export const API = `http://localhost:5000/`;
